39fa0725b564824b8639c7897a2ce75a58e933fa
festrati <festrati@gmail.com>
2015-02-17 23:40:49 -0200

    Add images Background Cannon

--- Space Cannon/GameScene.m
+++ Space Cannon/GameScene.m
@@ -9,36 +9,46 @@
 #import "GameScene.h"
 
 @implementation GameScene
+{
+    SKNode *_mainLayer;
+    SKSpriteNode *_cannon;
+}
 
 -(void)didMoveToView:(SKView *)view {
     /* Setup your scene here */
-    SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
     
-    myLabel.text = @"Hello, World!";
-    myLabel.fontSize = 65;
-    myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                   CGRectGetMidY(self.frame));
+    // adicionando bg
+    SKSpriteNode *background = [SKSpriteNode spriteNodeWithImageNamed:@"Starfield"];
+    background.position = CGPointMake(self.frame.size.width/2, self.frame.size.height/2);
+    background.size = CGSizeMake(self.size.width * 0.42, self.size.height);
+//    background.anchorPoint = CGPointZero;
+    background.blendMode = SKBlendModeReplace;
+    [self addChild:background];
     
-    [self addChild:myLabel];
+    
+    // add main layer
+    _mainLayer = [[SKNode alloc] init];
+    [self addChild:_mainLayer];
+    
+    
+    //add cannon
+    _cannon = [SKSpriteNode spriteNodeWithImageNamed:@"Cannon"];
+    _cannon.position = CGPointMake(self.size.width * 0.5, 0.0f );
+    [_mainLayer addChild:_cannon];
+  
+    //cannon action
+    SKAction *rotateCannon = [SKAction sequence:@[[SKAction rotateByAngle:M_PI duration:2],
+                                                  [SKAction rotateByAngle:-M_PI duration:2]]];
+    
+    [_cannon runAction:[SKAction repeatActionForever:rotateCannon]];
+    
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     /* Called when a touch begins */
     
     for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
         
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-        
-        sprite.xScale = 0.5;
-        sprite.yScale = 0.5;
-        sprite.position = location;
-        
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        
-        [sprite runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:sprite];
     }
 }
 
